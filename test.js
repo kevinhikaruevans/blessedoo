f77184dafdacd62876d6b95394d029733565e36d
Kevin Evans <hello@kevinevans.name>
2015-01-06 13:11:41 -0800

    test

--- test.js
+++ test.js
@@ -3,6 +3,9 @@ var blessedoo = require('./blessedoo')();
 var context = {
   doStuff: function() {
     console.log('asdf');
+  },
+  doStuff2: function() {
+    console.log('asdfgg');
   }
 };
 blessedoo.loadView('sections/test.xml', context, function(err, result) {
